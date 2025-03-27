f7a017a5910fa043809f51c2fc1d89c707f36ee4
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-03-27 19:33:59 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,8 +1,8 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        // System.out.println(array[0]);
+        
         int[] answer = new int[commands.length];
         
         for (int idx = 0; idx < commands.length; idx++) {
@@ -11,10 +11,10 @@ class Solution {
             int k = commands[idx][2];
             int[] slice = Arrays.copyOfRange(array, i-1, j);
             Arrays.sort(slice);
-            System.out.println(Arrays.toString(slice));
-            answer[idx] = slice[k-1]; 
+            answer[idx] = slice[k-1];
         }
         
         return answer;
+        
     }
 }

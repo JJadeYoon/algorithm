824caed8faa5901f9b6cc3b456da3942ebee6176
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-04-03 15:56:30 +0900

    [level 2] Title: 조이스틱, Time: 0.13 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -11,21 +11,15 @@ class Solution {
             char c = name.charAt(i);
             answer += Math.min(c - 'A', 'Z' - c + 1);
 
+            // i는 꺾는 위치
             // 다음 위치부터 연속된 A가 끝나는 지점 찾기
             int nextIndex = i + 1;
             while (nextIndex < length && name.charAt(nextIndex) == 'A') {
                 nextIndex++;
             }
 
-            // 기존 방식: 정방향 또는 역방향으로만 이동
             move = Math.min(move, i * 2 + (length - nextIndex)); // 뒤로 갔다가 처음부터 남은 거리
             move = Math.min(move, (length - nextIndex) * 2 + i); // 남은 거리 먼저 갔다가 처음부터 현재 위치까지
-
-            // 추가: 앞으로 갔다가 뒤로 돌아가는 경우 (AIAAAAZ 같은 경우)
-//            if (i > 0 && nextIndex < length) {
-//                // i까지 정방향으로 이동 + (length-nextIndex)까지 역방향으로 이동
-//                move = Math.min(move, i + 2 * (length - nextIndex));
-//            }
         }
 
         return answer + move;

19435b189580a3d0a611368df09c8610d5ba4309
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-04-24 14:10:20 +0900

    [level 3] Title: 등굣길, Time: 0.60 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -7,21 +7,21 @@ class Solution {
             puddleMap[p[0]][p[1]] = true;
         }
 
-        int[] dp = new int[n+1];
+        int[][] dp = new int[m+1][n+1];
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i == 1 && j == 1) {
-                    dp[j] = 1;
+                    dp[i][j] = 1;
                     continue;
                 }
                 // 웅덩이인지 확인
                 if (puddleMap[i][j]) {
-                    dp[j] = 0;
+                    dp[i][j] = 0;
                     continue;
                 }
-                dp[j] = (dp[j] + dp[j-1]) % MOD;
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
             }
         }
-        return dp[n];
+        return dp[m][n];
     }
 }

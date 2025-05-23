61872cb560a2392eb5a3bb4b741c05d150051245
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-05-23 21:38:57 +0900

    [level 2] Title: 가장 큰 수, Time: 246.65 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,26 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        
-        String[] stringNumbers = new String[numbers.length];
-        
+        String[] strNumbers = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            stringNumbers[i] = String.valueOf(numbers[i]);
+            strNumbers[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(stringNumbers, (a, b) ->
+        Arrays.sort(strNumbers, (a, b) -> 
                         Integer.parseInt(b + a) - Integer.parseInt(a + b)
                    );
         
-        if (stringNumbers[0].equals("0")) {
+        if (strNumbers[0].equals("0")) {
             return "0";
         }
         
         StringBuilder sb = new StringBuilder();
-        for (String s : stringNumbers) {
+        for (String s : strNumbers) {
             sb.append(s);
         }
-        
         return sb.toString();
     }
 }

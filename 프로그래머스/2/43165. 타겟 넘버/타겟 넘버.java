9ede63903182de14ee5f7b002058026fbef612b0
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-05-25 03:07:49 +0900

    [level 2] Title: 타겟 넘버, Time: 4.24 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int[] numbers, int target) {
-        return dfs(numbers, 0, 0, target);
-    }
 
-    private int dfs(int[] numbers, int n, int sum, int target) {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 1, numbers[0]) + dfs(numbers, target, 1, -numbers[0]);
+    }
+    
+    private int dfs(int[] numbers, int target, int n, int sum) {
         if (n == numbers.length) {
             if (sum == target) {
                 return 1;
+            } else {
+                return 0;
             }
-            return 0;
         }
-        return dfs(numbers, n + 1, sum + numbers[n], target)
-                + dfs(numbers, n + 1, sum - numbers[n], target);
+        return dfs(numbers, target, n + 1, sum + numbers[n]) + dfs(numbers, target, n + 1, sum - numbers[n]);
     }
 }

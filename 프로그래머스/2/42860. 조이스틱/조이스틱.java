8b4009436d02ed4ee8183c24f6d1630be0158868
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-08-18 01:05:30 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,27 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(String name) {
-        int answer = 0;
-        int length = name.length();
-
-        // 최대 이동 횟수 (왼쪽에서 오른쪽으로 쭉 이동)
-        int move = length - 1;
-
-        for (int i = 0; i < length; i++) {
-            // 상하 이동 계산
-            char c = name.charAt(i);
-            answer += Math.min(c - 'A', 'Z' - c + 1);
-
-            // i는 꺾는 위치
-            // 다음 위치부터 연속된 A가 끝나는 지점 찾기
-            int nextIndex = i + 1;
-            while (nextIndex < length && name.charAt(nextIndex) == 'A') {
-                nextIndex++;
-            }
-
-            move = Math.min(move, i * 2 + (length - nextIndex)); // 뒤로 갔다가 처음부터 남은 거리
-            move = Math.min(move, (length - nextIndex) * 2 + i); // 남은 거리 먼저 갔다가 처음부터 현재 위치까지
+        char[] target = name.toCharArray();
+        int n = target.length;
+        
+        int ud = 0;
+        for (int i = 0; i < n; i++) {
+            ud += Math.min(target[i] - 'A', 'Z' - target[i] + 1);
         }
-
-        return answer + move;
+        
+        int lr = n - 1;
+        for (int i = 0; i < n; i++) {
+            int next = i + 1;
+            
+            while (next < n && target[next] == 'A') {
+                next++;
+            }
+            
+            lr = Math.min(lr, i * 2 + n - next);
+            lr = Math.min(lr, (n - next) * 2 + i);
+        }
+        
+        int total = ud + lr;
+        return total;
     }
 }

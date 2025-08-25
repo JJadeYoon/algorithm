b510bc65552a5f32dde174666e434b6e252261bd
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-08-25 10:46:33 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 52.02 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,39 +2,46 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int n = gems.length;
-        Set<String> hs = new HashSet<>();
+        if (gems.length == 1) return new int[]{1, 1};
+        
+        // 고유한 보석 종류 수 계산
+        Set<String> gemTypes = new HashSet<>();
         for (String gem : gems) {
-            hs.add(gem);
+            gemTypes.add(gem);
         }
-        int m = hs.size();
-
-        Map<String, Integer> hm = new HashMap<>();
-
-        int[] answer = new int[]{1, n};
+        int requiredTypes = gemTypes.size();
+        
+        Map<String, Integer> windowCount = new HashMap<>();
+        int minLength = gems.length;
+        int resultStart = 0, resultEnd = gems.length - 1;
         
         int start = 0;
-        for (int end = 0; end < n; end++) {
-            String curr = gems[end];
-            int currCnt = hm.getOrDefault(curr, 0);
-            hm.put(curr, currCnt + 1);
-
-            if (hm.size() == m) {
-                while (hm.size() == m && start < n) {
-                    int temp = hm.getOrDefault(gems[start], 0);
-                    if (temp <= 1) {
-                        hm.remove(gems[start++]);
-                    } else {
-                        hm.put(gems[start++], temp - 1);
-                    }
+        for (int end = 0; end < gems.length; end++) {
+            // 윈도우 확장
+            String currentGem = gems[end];
+            windowCount.put(currentGem, windowCount.getOrDefault(currentGem, 0) + 1);
+            
+            // 모든 종류가 포함되면 윈도우 축소 시도
+            while (windowCount.size() == requiredTypes) {
+                int currentLength = end - start + 1;
+                if (currentLength < minLength) {
+                    minLength = currentLength;
+                    resultStart = start;
+                    resultEnd = end;
                 }
-                if (end + 1 - start < answer[1] - answer[0]) {
-                    answer = new int[]{start, end + 1};
+                
+                // 윈도우 축소
+                String leftGem = gems[start];
+                int leftCount = windowCount.get(leftGem) - 1;
+                if (leftCount == 0) {
+                    windowCount.remove(leftGem);
+                } else {
+                    windowCount.put(leftGem, leftCount);
                 }
-                // return new int[]{start + 1, end + 1};
+                start++;
             }
         }
-
-        return answer;
+        
+        return new int[]{resultStart + 1, resultEnd + 1};
     }
 }

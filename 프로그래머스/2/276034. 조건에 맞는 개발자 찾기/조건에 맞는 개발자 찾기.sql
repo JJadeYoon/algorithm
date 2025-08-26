f51eb36617e042891575496ce874ecfa792aa469
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-08-26 15:51:14 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,7 +1,6 @@
 -- 코드를 작성해주세요
-SELECT DISTINCT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
-FROM DEVELOPERS D
-    JOIN SKILLCODES S
-        ON S.NAME IN ('C#', 'Python')
-            AND (D.SKILL_CODE & S.CODE) = S.CODE
-ORDER BY 1;
+select d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+from DEVELOPERS d
+WHERE d.SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python')
+    or d.SKILL_CODE & (select CODE from SKILLCODES where NAME = 'C#')
+order by ID;

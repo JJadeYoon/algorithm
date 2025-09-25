1f8146611fe438c75f817c75bc4feb573ca5492a
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-09-26 02:01:30 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,23 +1,34 @@
 import java.util.*;
 
 class Solution {
+    
     public int solution(int n, int[][] computers) {
-        boolean[] isVisit = new boolean[n];
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            if (!isVisit[i]) {
-                dfs(isVisit, i, n, computers);
-                answer++;
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        
+        for (int i = 0; i < computers.length; i++) {
+            if (visited[i]) {
+                continue;
             }
+            
+            visited[i] = true;
+            count++;
+            dfs(computers, visited, i, n);
         }
-        return answer;
+        
+        return count;
     }
     
-    private void dfs(boolean[] isVisit, int index, int n, int[][] computers) {
-        isVisit[index] = true;
+    public void dfs(int[][] computers, boolean[] visited, int curr, int n) {
+        int[] conn = computers[curr];
         for (int i = 0; i < n; i++) {
-            if (computers[index][i] == 1 && !isVisit[i]) {
-                dfs(isVisit, i, n, computers);
+            if (conn[i] == 1 && i != curr) {
+                if (visited[i]) {
+                    continue;
+                }
+
+                visited[i] = true;
+                dfs(computers, visited, i, n);
             }
         }
     }

e92cd7395582e5742cddbcf1a6390d74cfc29b4e
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-09-27 01:50:17 +0900

    [level 2] Title: 배달, Time: 4.93 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 97.8 MB, 시간: 8.85 ms
+메모리: 76.5 MB, 시간: 4.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 24일 11:39:02
+2025년 09월 27일 01:50:16
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,68 +1,65 @@
 import java.util.*;
 
 class Solution {
-    private static class Edge {
-        int v;
-        int weight;
+    
+    class Edge {
+        int v, d;
         
-        Edge(int v, int weight) {
+        public Edge(int v, int d) {
             this.v = v;
-            this.weight = weight;
+            this.d = d;
         }
     }
     
-    private List<Edge>[] graph;
-    private int[] dist;
-    private boolean[] visited;
-    
     public int solution(int N, int[][] road, int K) {
-        graph = new List[N + 1];
-        dist = new int[N + 1];
-        visited = new boolean[N + 1];
+        List<Edge>[] edges = new List[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        int[] dist = new int[N + 1];
         
         for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
+            edges[i] = new ArrayList<Edge>();
             dist[i] = Integer.MAX_VALUE;
         }
         
         for (int[] r : road) {
-            int inputU = r[0];
-            int inputV = r[1];
-            int inputW = r[2];
+            int u = r[0];
+            int v = r[1];
+            int d = r[2];
             
-            graph[inputU].add(new Edge(inputV, inputW));
-            graph[inputV].add(new Edge(inputU, inputW));
+            edges[u].add(new Edge(v, d));
+            edges[v].add(new Edge(u, d));
         }
         
-        dijkstra(1);
+        dijkstra(1, edges, dist, visited);
         
-        int answer = 0;
-        for (int i = 1; i < dist.length; i++) {
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
             if (dist[i] <= K) {
-                answer++;
+                count++;
             }
         }
-        return answer;
+        
+        return count;
     }
     
-    private void dijkstra(int start) {
+    private void dijkstra(int start, List<Edge>[] edges, int[] dist, boolean[] visited) {
         PriorityQueue<Edge> pq = new PriorityQueue<>(
-            (o1, o2) -> o1.weight - o2.weight
-        );
-        
+            (e1, e2) -> {
+                return e1.d - e2.d;
+            });
         pq.offer(new Edge(start, 0));
         dist[start] = 0;
         
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             Edge curr = pq.poll();
             
             if (!visited[curr.v]) {
                 visited[curr.v] = true;
             }
             
-            for (Edge next : graph[curr.v]) {
-                if (!visited[next.v] && dist[next.v] > curr.weight + next.weight) {
-                    dist[next.v] = curr.weight + next.weight;
+            for (Edge next : edges[curr.v]) {
+                if (!visited[next.v] && dist[next.v] > curr.d + next.d) {
+                    dist[next.v] = curr.d + next.d;
                     pq.offer(new Edge(next.v, dist[next.v]));
                 }
             }

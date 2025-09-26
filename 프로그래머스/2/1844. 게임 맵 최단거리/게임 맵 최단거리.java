4e70772bd3eb340595fadce84d657ef5c5ea85fb
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-09-27 00:41:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.65 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,66 @@
 import java.util.*;
 
 class Solution {
-    static class Point {
-        int x = 0;
-        int y = 0;
-        int distance = 0;
-        
-        Point(int x, int y, int distance) {
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
-        }
-    }
+    
+    private static final int[][] directions = {
+        {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+    };
     
     public int solution(int[][] maps) {
-        int m = maps.length;
-        int n = maps[0].length;
         
-        boolean[][] visited = new boolean[m][n];
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        boolean[][] visited = new boolean[n][m];
+        
+        Queue<Integer> xQ = new ArrayDeque<>();
+        Queue<Integer> yQ = new ArrayDeque<>();
+        Queue<Integer> tQ = new ArrayDeque<>();
+        
+        xQ.offer(0);
+        yQ.offer(0);
+        tQ.offer(1);
         visited[0][0] = true;
         
-        int[][] directions = {
-            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
-        };
-        
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0, 0, 1));
-        
-        while (!queue.isEmpty()) {
-            Point curr = queue.poll();
-            if (curr.x == m - 1 && curr.y == n - 1) {
-                return curr.distance;
+        while (!xQ.isEmpty()) {
+            int currX = xQ.poll();
+            int currY = yQ.poll();
+            int currT = tQ.poll();
+            
+            if (currX == n - 1 && currY == m - 1) {
+                return currT;
             }
-            for (int[] d : directions) {
-                int x = curr.x + d[0];
-                int y = curr.y + d[1];
-                if (x < m && x > -1 && y < n && y > -1 && !visited[x][y] && maps[x][y] == 1) {
-                    queue.offer(new Point(x, y, curr.distance + 1));
-                    visited[x][y] = true;
+            
+            for (int[] dir : directions) {
+                int nextX = currX + dir[0];
+                int nextY = currY + dir[1];
+                
+                if (isValid(maps, visited, nextX, nextY)) {
+                    xQ.offer(nextX);
+                    yQ.offer(nextY);
+                    tQ.offer(currT + 1);
+                    visited[nextX][nextY] = true;
                 }
             }
         }
+        
         return -1;
+    }
+    
+    private boolean isValid(int[][] maps, boolean[][] visited, int x, int y) {
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        if (x < 0 || y < 0 || x >= n || y >= m) {
+            return false;
+        }
+        if (maps[x][y] == 0) {
+            return false;
+        }
+        if (visited[x][y]) {
+            return false;
+        }
+        
+        return true;
     }
 }

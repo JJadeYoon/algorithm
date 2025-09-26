11fb8a4c059464234865aab096bd172b87ecedf5
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-09-27 02:08:00 +0900

    [level 2] Title: 배달, Time: 7.43 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -3,63 +3,61 @@ import java.util.*;
 class Solution {
     
     class Edge {
-        int v, d;
+        int v, w;
         
-        public Edge(int v, int d) {
+        Edge(int v, int w) {
             this.v = v;
-            this.d = d;
+            this.w = w;
         }
     }
     
-    public int solution(int N, int[][] road, int K) {
+    public int solution(int N, int[][] roads, int K) {
         List<Edge>[] edges = new List[N + 1];
         boolean[] visited = new boolean[N + 1];
         int[] dist = new int[N + 1];
         
         for (int i = 1; i <= N; i++) {
-            edges[i] = new ArrayList<Edge>();
+            edges[i] = new ArrayList<>();
             dist[i] = Integer.MAX_VALUE;
         }
         
-        for (int[] r : road) {
+        for (int[] r : roads) {
             int u = r[0];
             int v = r[1];
-            int d = r[2];
+            int w = r[2];
             
-            edges[u].add(new Edge(v, d));
-            edges[v].add(new Edge(u, d));
+            edges[u].add(new Edge(v, w));
+            edges[v].add(new Edge(u, w));
         }
         
-        dijkstra(1, edges, dist, visited);
+        dijkstra(edges, visited, dist, 1);
         
-        int count = 0;
+        int answer = 0;
         for (int i = 1; i <= N; i++) {
             if (dist[i] <= K) {
-                count++;
+                answer++;
             }
         }
         
-        return count;
+        return answer;
     }
     
-    private void dijkstra(int start, List<Edge>[] edges, int[] dist, boolean[] visited) {
+    private void dijkstra(List<Edge>[] edges, boolean[] visited, int[] dist, int start) {
         PriorityQueue<Edge> pq = new PriorityQueue<>(
-            (e1, e2) -> {
-                return e1.d - e2.d;
-            });
+            (e1, e2) -> { return e1.w - e2.w; }
+        );
         pq.offer(new Edge(start, 0));
         dist[start] = 0;
         
         while (!pq.isEmpty()) {
             Edge curr = pq.poll();
-            
             if (!visited[curr.v]) {
                 visited[curr.v] = true;
             }
             
             for (Edge next : edges[curr.v]) {
-                if (!visited[next.v] && dist[next.v] > curr.d + next.d) {
-                    dist[next.v] = curr.d + next.d;
+                if (!visited[next.v] && curr.w + next.w < dist[next.v]) {
+                    dist[next.v] = curr.w + next.w;
                     pq.offer(new Edge(next.v, dist[next.v]));
                 }
             }

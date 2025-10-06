de0adf0a92daae9faa0499a4268812c07eb2adc5
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-10-06 21:40:44 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 22.65 ms, Memory: 98.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        Map<String, Integer> ni = new HashMap<>();
+        for (int i = 0; i < friends.length; i++) {
+            ni.put(friends[i], i);
+        }
+        
+        int[][] records = new int[friends.length][friends.length];
+        StringTokenizer st;
+        for (String gift : gifts) {
+            st = new StringTokenizer(gift);
+            int g = ni.get(st.nextToken());
+            int r = ni.get(st.nextToken());
+            records[g][r]++;
+        }
+        
+        int[] scores = new int[friends.length];
+        for (int i = 0; i < friends.length; i++) {
+            int give = 0;
+            for (int g : records[i]) {
+                give += g;
+            }
+            
+            int receive = 0;
+            for (int j = 0; j < friends.length; j++) {
+                receive += records[j][i];
+            }
+            
+            scores[i] = give - receive;
+        }
+        
+        int[] answers = new int[friends.length];
+        for (int i = 0; i < friends.length; i++) {            
+            int next = 0;
+            
+            for (int j = 0; j < friends.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                
+                if (records[i][j] > records[j][i]) {
+                    next++;
+                } else if (records[i][j] == records[j][i] 
+                           && scores[i] > scores[j]) {
+                    next++;
+                }
+            }
+            
+            answers[i] = next;
+        }
+        
+        Arrays.sort(answers);
+        return answers[answers.length - 1];
+    }
+}

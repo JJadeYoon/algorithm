04adc6c5e011b3c172bdb0c7287d8e779bdcdb49
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-10-28 16:00:16 +0900

    [level 3] Title: 가장 먼 노드, Time: 53.02 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    
+    class Edge {
+        int from, to;
+        Edge(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+    }
+    
+    public int solution(int n, int[][] edge) {
+        List<Edge>[] edges = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            edges[i] = new ArrayList<>();
+        }
+        
+        for (int[] e : edge) {
+            int from = e[0];
+            int to = e[1];
+            edges[from].add(new Edge(from, to));
+            edges[to].add(new Edge(to, from));
+        }
+        
+        int[] distance = new int[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        distance[1] = 0;
+        queue.offer(new int[]{1, 0});
+        
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            
+            for (Edge e : edges[curr[0]]) {
+                if (distance[e.to] <= curr[1] + 1) {
+                    continue;
+                }
+                distance[e.to] = curr[1] + 1;
+                queue.offer(new int[]{e.to, curr[1] + 1});
+            }
+        }
+        
+        System.out.println(Arrays.toString(distance));
+        
+        int maxDist = distance[1];
+        for (int i = 2; i <= n; i++) {
+            maxDist = Math.max(maxDist, distance[i]);
+        }
+        
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (distance[i] == maxDist) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}

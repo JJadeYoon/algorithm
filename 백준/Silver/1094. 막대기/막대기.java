28bd245e2a680d4f98d7bfd4f1694541ddb0dde2
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-10-31 13:38:01 +0900

    [Silver V] Title: 막대기, Time: 168 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -5,37 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count = 64;
-        while (count != n) {
-            if (count > n) {
-                count -= getMinSize(count) / 2;
-            } else {
-                count += getMinSize(count) / 2;
-            }
+        System.out.print(Integer.bitCount(n));
         }
-        System.out.print(countUsed(count));
-    }
-
-    private static int countUsed(int n) {
-        int count = 0;
-        int mask = 1;
-        while (mask <= 64) {
-            if ((mask & n) != 0) {
-                count++;
-            }
-            mask = mask << 1;
-        }
-        return count;
-    }
-
-    private static int getMinSize(int n) {
-        int minSize = 1;
-        while (minSize < n) {
-            if ((n & minSize) > 0) {
-                break;
-            }
-            minSize = minSize << 1;
-        }
-        return minSize;
-    }
 }

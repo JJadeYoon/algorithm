47a6bb00abc3056d4aed6ad32018ec4685e1730a
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-11-08 00:05:09 +0900

    [level 3] Title: 등굣길, Time: 0.80 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,27 +1,51 @@
-class Solution {
-    public int solution(int m, int n, int[][] puddles) {
-        int MOD = 1000000007;
+import java.util.*;
 
-        boolean[][] puddleMap = new boolean[m+1][n+1];
+class Solution {
+    static final int MOD = 1_000_000_007;
+
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] map = new int[n][m];
+
         for (int[] p : puddles) {
-            puddleMap[p[0]][p[1]] = true;
+            int x = p[0] - 1;
+            int y = p[1] - 1;
+            map[y][x] = -1;
         }
 
-        int[][] dp = new int[m+1][n+1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == 1 && j == 1) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-                // 웅덩이인지 확인
-                if (puddleMap[i][j]) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
+        if (map[0][0] == -1) return 0;
+        map[0][0] = 1;
+
+        // 첫 행
+        for (int j = 1; j < m; j++) {
+            if (map[0][j] == -1) {
+                map[0][j] = 0;
+            } else {
+                map[0][j] = map[0][j - 1];
             }
         }
-        return dp[m][n];
+
+        // 첫 열
+        for (int i = 1; i < n; i++) {
+            if (map[i][0] == -1) {
+                map[i][0] = 0;
+            } else {
+                map[i][0] = map[i - 1][0];
+            }
+        }
+
+        // 나머지
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (map[i][j] == -1) {
+                    map[i][j] = 0;
+                    continue;
+                }
+                long up = map[i - 1][j];
+                long left = map[i][j - 1];
+                map[i][j] = (int) ((up + left) % MOD);
+            }
+        }
+
+        return map[n - 1][m - 1];
     }
 }

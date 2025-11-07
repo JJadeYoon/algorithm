62c81d6b64903eb65dc12be467d5bb60bc5575da
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-11-07 21:01:48 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 444 ms, Memory: 48436 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,96 +1,75 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
-class Main {
-    private static class Edge implements Comparable<Edge> {
-        int u, v, w;
+public class Main {
 
-        Edge(int u, int v, int w) {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static class Edge {
+        int u;
+        int v;
+        int w;
+
+        public Edge(int u, int v, int w) {
             this.u = u;
             this.v = v;
             this.w = w;
         }
-
-        @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(this.w, o.w);
-        }
     }
 
-    private static class UnionFind {
-        int[] p;
+    private static int[] p;
 
-        UnionFind(int n) {
-            p = new int[n + 1];
-            for (int i = 1; i <= n; i++) {
-                p[i] = -1;
-            }
-        }
-
-        int find(int x) {
-            if (p[x] < 0) {
-                return x;
-            }
-            return p[x] = find(p[x]);
-        }
-
-        boolean union(int u, int v) {
-            u = find(u);
-            v = find(v);
-
-            if (u == v) {
-                return false;
-            }
-
-            if (p[u] > p[v]) {
-                p[u] = v;
-            } else if (p[u] < p[v]) {
-                p[v] = u;
-            } else {
-                p[v] = u;
-                p[u]--;
-            }
-
-            return true;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        List<Edge> edges = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
+        p = new int[V + 1];
+        Arrays.fill(p, -1);
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>(
+            (e1, e2) -> {
+                return e1.w - e2.w;}
+        );
+
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(u, v, w));
+            pq.offer(new Edge(u, v, w));
         }
 
-        System.out.print(kruskal(n, edges));
-    }
-
-    private static int kruskal(int n, List<Edge> edges) {
-        Collections.sort(edges);
-
-        UnionFind uf = new UnionFind(n);
-        List<Edge> mst = new ArrayList<>();
-        int totalWeight = 0;
-
-        for (Edge edge : edges) {
-            if (uf.union(edge.u, edge.v)) {
-                mst.add(edge);
-                totalWeight += edge.w;
-
-                if (mst.size() == n - 1) {
-                    break;
-                }
+        int total = 0;
+        while (!pq.isEmpty()) {
+            Edge e = pq.poll();
+            if (find(e.u) != find(e.v)) {
+                total += e.w;
+                union(e.u, e.v);
             }
         }
 
-        return mst.size() == n - 1 ? totalWeight : -1;
+        System.out.print(total);
+    }
+
+    private static int find(int u) {
+        if (p[u] < 0) {
+            return u;
+        }
+        return p[u] = find(p[u]);
+    }
+
+    private static boolean union(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u == v) {
+            return false;
+        }
+        p[v] = u;
+        return true;
     }
 }

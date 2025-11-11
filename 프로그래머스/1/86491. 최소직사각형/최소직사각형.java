360ad8df68de0bf0410048ee8f0244fac8ac1593
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-11-11 14:59:56 +0900

    [level 1] Title: 최소직사각형, Time: 3.44 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,17 +1,25 @@
+import java.util.*;
+
 class Solution {
+    // 명함 정렬
+    /*
+    50 60
+    30 70
+    30 60
+    40 80
+    */
     public int solution(int[][] sizes) {
-        int maxWidth = 0;
-        int maxHeight = 0;
- 
-        for (int[] size : sizes) {
- 
-            int width = Math.max(size[0], size[1]);
-            int height = Math.min(size[0], size[1]);
- 
-            maxWidth = Math.max(maxWidth, width);
-            maxHeight = Math.max(maxHeight, height);
+        for (int[] s : sizes) {
+            Arrays.sort(s);
         }
- 
-        return maxWidth * maxHeight;
+        
+        int w = sizes[0][0];
+        int h = sizes[0][1];
+        for (int i = 1; i < sizes.length; i++) {
+            w = Math.max(w, sizes[i][0]);
+            h = Math.max(h, sizes[i][1]);
+        }
+        
+        return w * h;
     }
 }

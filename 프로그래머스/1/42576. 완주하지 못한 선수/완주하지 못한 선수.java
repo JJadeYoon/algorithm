d12585af12143dc4b84608b759c073ff0776222e
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2026-04-08 00:02:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.29 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,17 +3,22 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> hm = new HashMap<>();
+        
         for (String p : participant) {
             hm.put(p, hm.getOrDefault(p, 0) + 1);
         }
+        
         for (String c : completion) {
-            hm.put(c, hm.getOrDefault(c, 0) - 1);
+            hm.put(c, hm.get(c) - 1);
         }
-        for (Map.Entry<String, Integer> e : hm.entrySet()) {
-            if (e.getValue() > 0) {
-                return e.getKey();
+        
+        for (String s : hm.keySet()) {
+            int v = hm.get(s);
+            if (v > 0) {
+                return s;
             }
         }
+        
         return "";
     }
 }

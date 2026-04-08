96e96cb11cfe6014ac643a0ae266a53342460947
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2026-04-08 22:50:13 +0900

    [level 2] Title: 기능개발, Time: 0.22 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,29 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int n = progresses.length;
-        List<Integer> answer = new ArrayList<>();
         Queue<Integer> queue = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i++) {
-            int remains = 100 - progresses[i];
-            int days = remains / speeds[i];
-            if (remains % speeds[i] != 0) {
-                days++;
+        for (int i = 0; i < progresses.length; i++) {
+            int remain = 100 - progresses[i];
+            int day = remain / speeds[i];
+            if (remain % speeds[i] != 0) {
+                day++;
             }
-            queue.offer(days);
+            queue.offer(day);
         }
         
+        List<Integer> list = new ArrayList<>();
         while (!queue.isEmpty()) {
             int curr = queue.poll();
-            int count = 1;
-            while (!queue.isEmpty() && queue.peek() <= curr) {
+            int cnt = 1;
+            while (!queue.isEmpty() && curr >= queue.peek()) {
                 queue.poll();
-                count++;
+                cnt++;
             }
-            answer.add(count);
+            list.add(cnt);
         }
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
     }
 }

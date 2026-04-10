72b5339b9891a0bb1f0298b223489a47311b548e
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2026-04-10 22:48:10 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 7.91 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,58 +1,35 @@
 import java.util.*;
 
 class Solution {
-    
-    static class Request {
-        int num;
-        int arrive;
-        int duration;
-        
-        public Request(int num, int arrive, int duration) {
-            this.num = num;
-            this.arrive = arrive;
-            this.duration = duration;
-        }
-    }
-    
     public int solution(int[][] jobs) {
-        // jobs를 도착 시간 순으로 정렬
-        Request[] r = new Request[jobs.length];
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        
+        // n번째, 요청 시간, 소요 시간
+        Queue<int[]> queue = new ArrayDeque<>();
         for (int i = 0; i < jobs.length; i++) {
-            int[] job = jobs[i];
-            r[i] = new Request(i, job[0], job[1]);
-        }
-        Arrays.sort(r, (a, b) -> Integer.compare(a.arrive, b.arrive));
-        
-        Queue<Request> queue = new LinkedList<>();
-        for (Request req : r) {
-            queue.offer(req);
+            queue.offer(new int[]{i, jobs[i][0], jobs[i][1]}); 
         }
         
-        PriorityQueue<Request> pq = new PriorityQueue<>(
-            Comparator.<Request>comparingInt(req -> req.duration)
-            .thenComparingInt(req -> req.arrive)
-            .thenComparingInt(req -> req.num)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> a[2] != b[2] ? a[2] - b[2] : a[0] - b[0]
         );
         
-        int time = 0;
-        int totalTime = 0;
-        while(!queue.isEmpty() || !pq.isEmpty()) {
-            // 현재 시간까지 도착한 작업들을 pq에 추가
-            while(!queue.isEmpty() && queue.peek().arrive <= time) {
+        int t = 0;
+        int total = 0;
+        while (!queue.isEmpty() || !pq.isEmpty()) {
+            while (!queue.isEmpty() && queue.peek()[1] <= t) {
                 pq.offer(queue.poll());
             }
             
             if (pq.isEmpty()) {
-                // 실행할 작업이 없으면 다음 작업 도착 시간으로 점프
-                time = queue.peek().arrive;
+                t = queue.peek()[1];
             } else {
-                // 우선순위가 가장 높은 작업 하나만 처리
-                Request curReq = pq.poll();
-                totalTime += time + curReq.duration - curReq.arrive;
-                time += curReq.duration;
+                int[] curr = pq.poll();
+                total += t + curr[2] - curr[1];
+                t += curr[2];
             }
         }
         
-        return totalTime / jobs.length;
+        return total / jobs.length;
     }
 }
